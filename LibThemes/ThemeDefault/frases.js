02d18bf5bc6e4c0b8c5e5a2ab35f939ab0ab887b
thebighead <thebighead@abase>
2015-10-28 03:58:04 -0200

    Preparando o chá

---
+++ LibThemes/ThemeDefault/frases.js
@@ -0,0 +1,10 @@
+<script language='JavaScript' type='text/JavaScript'>
+    function makeQuote() {
+    Q = new Array();
+    Q[0] = "Assim como o sol derrete o gelo, a gentileza evapora mal-entendidos, desconfianças e hostilidade.Albert Schweitzer"
+    Q[1] = "<i>É impossível fazer qualquer coisa à prova de tolos, porque os tolos são muito engenhosos.</i>Arthur Bloch"
+    Q[2] = "<i>Quem ama crê no impossível.</i> Elisabeth B. Browning"
+    index = Math.floor(Math.random() * Q.length);
+    document.writeln(Q[index]);
+    }
+</script> 
